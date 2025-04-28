f6e5f7d9ecff507ace4fe39bfeab7b2221f30178
Javier Alemán <108713280+alemanjavier@users.noreply.github.com>
2025-04-28 14:04:52 -0400

    Create aya.py

---
+++ aya.py
@@ -0,0 +1,30 @@
+import board
+import time
+import displayio
+from adafruit_matrixportal.matrixportal import MatrixPortal
+from adafruit_display_text.label import Label
+from adafruit_bitmap_font import bitmap_font
+
+matrixportal = MatrixPortal(status_neopixel=board.NEOPIXEL, bit_depth=6)
+small_font = bitmap_font.load_font("/fonts/5x8.bdf")
+
+main_group = displayio.Group()
+
+text_label = Label(
+    small_font,
+    text="Aya",
+    color=0xFF0000  # Red
+)
+
+display_width = matrixportal.graphics.display.width
+text_label.x = display_width
+text_label.y = 4 #needs to be 4
+
+main_group.append(text_label)
+matrixportal.graphics.display.root_group = main_group
+
+while True:
+    text_label.x -= 1
+    if (text_label.x + text_label.bounding_box[2]) < 0:
+        text_label.x = display_width
+    time.sleep(0.08)
